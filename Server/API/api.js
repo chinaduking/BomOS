65b0fe397a7953e13e0165fbd5187905af35ef6f
wangjin <jin.wang@ninebot.com>
2017-10-18 18:50:08 +0800

    user api finish

--- Server/API/api.js
+++ Server/API/api.js
@@ -1,40 +1,49 @@
 var jwt = require('jwt-simple');
 
 var API = {
-    Test: function Test(req,res) {
-        console.log('hello');
+
+    getAllUser: function getAllUser(req,res) {
         req.models.t_user.find().exec(function(err,result) {
             "use strict";
-            if(err == null){
-                console.log('success');
-            }else
-            {
-                console.log('err');
-            }
             res.json({Result: result,error: err});
         });
     },
 
-    getPosts: function getPosts(req,res) {
+    addUser: function addUser(req,res) {
+        var User = {
+            username:req.body.params.username,
+            password:req.body.params.password,
+            admin: 2,
+            avatar: 'default.jpg'
+        }
 
-        var curPage = req.query.curPage;
-        var limit = req.query.limit;
-        var tpage = 0;
+        req.models.t_user.create(User).exec(function(err,result) {
+            res.json({Result: result, error: err});
+        });
 
-        req.models.post.find().exec(function(err,result) {
-            if(err == null){
-                if(result.length % limit == 0)
-                    tpage = parseInt(result.length/limit);
-                else
-                    tpage = parseInt(result.length/limit) + 1;
-            }
-            res.json({Result: result,totalPage: tpage, error: err});
+    },
+
+    delUser:function delUser(req,res) {
+        var username = req.body.params.username;
+        req.models.t_user.destroy().where({'username':username}).exec(function(err,result) {
+            res.json({Result: result, error: err});
+        });
+    },
+
+    upPasswd:function upPasswd(req,res) {
+        var username = req.body.params.username;
+        var newpassword = req.body.params.password;
+        var  User = {
+            password:newpassword
+        };
+        req.models.post.update(User).where({'username':username}).exec(function(err,result) {
+            res.json({Result: result, error: err});
         });
     },
 
     Login: function(req,res) {
-        username = req.body.params.username,
-            password = req.body.params.password;
+        var username = req.body.params.username;
+        var password = req.body.params.password;
         if(username == 'wangjin' && password == '6db042d1203dd2873f2abb3a5c0bb3a9')
         {
             var token = jwt.encode({
@@ -58,41 +67,6 @@ var API = {
             })
         }
     },
-
-    addPost:function(req,res) {
-        var  Post = {
-            title:req.body.params.article.title,
-            author:req.body.params.article.author,
-            url:req.body.params.article.url,
-            content:req.body.params.article.content,
-        };
-
-        req.models.post.create(Post).exec(function(err,result) {
-            res.json({Result: result, error: err});
-        });
-    },
-
-    delPost:function(req,res) {
-        var index = req.body.params.index;
-        req.models.post.destroy(index).exec(function(err,result) {
-            res.json({Result: result, error: err});
-        });
-    },
-
-    upPost:function(req,res){
-        var index = req.body.params.index;
-        console.log(req.body.params);
-        var  Post = {
-            title:req.body.params.article.title,
-            author:req.body.params.article.author,
-            url:req.body.params.article.url,
-            content:req.body.params.article.content,
-        };
-        req.models.post.update(index,Post).exec(function(err,result) {
-            res.json({Result: result, error: err});
-        });
-    }
-
 }
 
 module.exports =  API;
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-//var UserController = require('../app/controllers/UserController');
 var Api = require('../API/Api');
 
-router.get('/getposts',Api.getPosts);
-router.get('/test',Api.Test);
 router.post('/login',Api.Login);
-router.post('/addpost',Api.addPost);
-router.post('/delpost',Api.delPost);
-router.post('/updatapost',Api.upPost);
+
+router.get('/getalluser',Api.getAllUser);
+router.post('/adduser',Api.addUser);
+router.post('/deluser',Api.delUser);
+router.post('/uppasswd',Api.upPasswd);
 
 module.exports = router;
